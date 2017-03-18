678e36683ec5198e8d43be53eaa4c6a3bc46d924
Maksim Lamansliy <maksim.lamanskiy@area9.dk>
2017-03-18 15:54:35 +0700

    Refactor HelperBase

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by user on 18.03.2017.
+ */
+public class NavigationHelper extends HelperBase {
+
+   public NavigationHelper(FirefoxDriver wd) {
+      super(wd);
+   }
+
+   public void gotoGroupPage() {
+      click(By.linkText("groups"));
+   }
+
+}
