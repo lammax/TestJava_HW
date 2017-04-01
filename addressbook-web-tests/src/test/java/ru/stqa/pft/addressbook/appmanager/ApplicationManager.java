4fda3bb84f3d68b6c723a8cc7a0a45534da4d2c7
Maksim Lamansliy <maksim.lamanskiy@area9.dk>
2017-04-01 11:07:59 +0700

    Changes for webdriver

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,22 +1,39 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
-   FirefoxDriver wd;
+   WebDriver wd;
+   String browser = BrowserType.FIREFOX;
 
    private SessionHelper sessionHelper;
    private NavigationHelper navigationHelper;
    private GroupHelper groupHelper;
    private ContactHelper contactHelper;
 
+   public ApplicationManager(String browser) {
+      this.browser = browser;
+   }
+
 
    public void init() {
-      wd = new FirefoxDriver();
+
+      if (browser.equals(BrowserType.FIREFOX)) {
+         wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)) {
+         wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+         wd = new InternetExplorerDriver();
+      }
+
       wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
       wd.get("http://localhost/addressbook/");
       groupHelper = new GroupHelper(wd);
