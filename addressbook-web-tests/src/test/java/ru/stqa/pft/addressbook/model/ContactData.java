f7994c41c3e894683ca39e6ec6422215e669ead0
Maksim Lamansliy <maksim.lamanskiy@area9.dk>
2017-04-26 09:37:00 +0700

    Added Hibernate for ContactData

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,45 +3,99 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 
 @XStreamAlias("contact")
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
    @XStreamOmitField
+   @Id
+   @Column(name = "id")
    private int id = Integer.MAX_VALUE;;
+
    @Expose
+   @Column(name = "firstname")
    private String name;
+
    @Expose
+   @Column(name = "lastname")
    private String lastname;
+
    @Expose
+   @Column(name = "nickname", length = 65535)
+//   @Type(type = "text")
    private String nick;
+
    @Expose
+   @Column(name = "company")
    private String company;
+
    @Expose
+   @Column(name = "mobile")
+   @Type(type = "text")
    private String mobile;
+
+   @Column(name = "home")
+   @Type(type = "text")
    private String homeTel;
+
+   @Column(name = "work")
+   @Type(type = "text")
    private String workTel;
+
+   @Transient
    private String allPhones;
+
    @Expose
+   @Column(name = "email")
+   @Type(type = "text")
    private String emaill;
+
+   @Column(name = "email2")
+   @Type(type = "text")
    private String email2;
+
+   @Column(name = "email3")
+   @Type(type = "text")
    private String email3;
+
+   @Transient
    private String allEmails;
+
    @Expose
+   @Column(name = "homepage")
+   @Type(type = "text")
    private String homepage;
+
    @Expose
+   @Column(name = "byear", length = 65535)
+//   @Type(type = "text")
    private String year;
+
    @Expose
+   @Column(name = "address")
+   @Type(type = "text")
    private String address;
+
    @Expose
+   @Column(name = "title", length = 65535)
+//   @Type(type = "text")
    private String title;
+
    @Expose
+   @Transient
    private String group;
-   private File photo;
+
+   @Column(name = "photo")
+   @Type(type = "text")
+   private String photo;
 
    public File getPhoto() {
-      return photo;
+      return new File(photo);
    }
 
    public String getAllEmails() {
@@ -206,7 +260,7 @@ public class ContactData {
    }
 
    public ContactData withPhoto(File photo) {
-      this.photo = photo;
+      this.photo = photo.getPath();
       return this;
    }
 
