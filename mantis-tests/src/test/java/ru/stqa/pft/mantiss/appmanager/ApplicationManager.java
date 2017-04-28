ef6c72cab547fc2d4f6ffd757632ac04f6dfcbc1
Maksim Lamansliy <maksim.lamanskiy@area9.dk>
2017-04-28 10:55:59 +0700

    Added new Mantiss tests module

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantiss/appmanager/ApplicationManager.java
@@ -0,0 +1,68 @@
+package ru.stqa.pft.mantiss.appmanager;
+
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+
+   private final Properties properties;
+   WebDriver wd;
+   String browser = BrowserType.FIREFOX;
+
+   public ApplicationManager(String browser) {
+      this.browser = browser;
+      properties = new Properties();
+   }
+
+
+   public void init() throws IOException {
+
+      String target = System.getProperty("target", "local");
+      properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+      if (browser.equals(BrowserType.FIREFOX)) {
+         wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)) {
+         wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+         wd = new InternetExplorerDriver();
+      }
+
+      wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+      wd.get(properties.getProperty("web.baseUrl"));
+   }
+
+   public Properties properties() {
+      return properties;
+   }
+
+   public void stop() {
+      wd.quit();
+   }
+
+   public boolean isAlertPresent() {
+      try {
+         wd.switchTo().alert();
+         return true;
+      } catch (NoAlertPresentException e) {
+         return false;
+      }
+   }
+
+   public void clickAllert() {
+      if (isAlertPresent()) {
+         wd.switchTo().alert().accept();
+      }
+   }
+
+}
