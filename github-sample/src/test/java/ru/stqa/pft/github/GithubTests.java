68bf10a5c580a20121e934518bac71cbd763a4e2
Maksim Lamansliy <maksim.lamanskiy@area9.dk>
2017-05-11 12:51:06 +0700

    Github rest test

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+   @Test
+   public void testCommits() throws IOException {
+      Github github = new RtGithub("977d33818617e1ca6969579ca5ffffcc6dea3a79");
+      RepoCommits commits = github.repos().get(new Coordinates.Simple("lammax", "TestJava_HW")).commits();
+      for(RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+         System.out.println(new RepoCommit.Smart(commit).message());
+      }
+
+   }
+
+}
